b8118a07f3793a503c4203ea88366f9d1aae80ab
Christoph Dieterich <christoph.dieterich@age.mpg.de>
2022-01-13 10:55:34 +0100

    Command line in step 3/4

---
+++ Nanopore_HEK293/Code/HEK293_data_prep_step4.R
@@ -0,0 +1,33 @@
+#library(NMF)
+#BigTable=readRDS("BigTable.rds")
+#res=readRDS("NMF.rds");
+
+AllSitesScores=readRDS("ScoreProfile_NMFall_plusNonCLIP.rds")
+
+# The palette with black:
+cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+Noverlap=read.delim("/Volumes//prj/JACUSA2_TestField/Nanopore_HEK293/miCLIP2/miCLIP_union_flat_exclude_Y_chromosome.bed",header=F,as.is=T)
+Noverlap.df=data.frame(ID=paste0(Noverlap[,1],":",Noverlap[,2]-2,"_",Noverlap[,3]+2,":",Noverlap[,6]),Type=Noverlap[,4])
+rownames(Noverlap.df)=Noverlap.df[,1]
+
+ins=intersect(rownames(AllSitesScores),Noverlap.df$ID);
+
+dataGG=data.frame(AllSitesScores[ins,],Noverlap.df[ins,2])
+NumberOfFactors=ncol(AllSitesScores)-2
+colnames(dataGG)=c(paste("NMF",1:NumberOfFactors,sep=""),"CLIP")
+
+library(ggplot2)
+
+for(dude in colnames(dataGG)[1:NumberOfFactors])
+    {
+xdensity <- ggplot(dataGG, aes_string(dude, color="CLIP")) + 
+stat_ecdf() +
+scale_color_manual(values = cbbPalette) + 
+theme_bw()
+
+#critical need to check 
+ggsave(paste0(dude,"_ecdf.pdf"),device="pdf")
+}
+
+#19:3976465-3976470:- ##

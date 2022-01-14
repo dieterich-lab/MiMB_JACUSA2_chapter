de99e40a8e456eaaa6814e11df2fe4601fc9503b
Christoph Dieterich <christoph.dieterich@age.mpg.de>
2022-01-14 16:28:20 +0100

    Command line in step 3/4

--- Nanopore_HEK293/Code/HEK293_data_prep_step4.R
+++ Nanopore_HEK293/Code/HEK293_data_prep_step4.R
@@ -7,7 +7,7 @@ AllSitesScores=readRDS("ScoreProfile_NMFall_plusNonCLIP.rds")
 # The palette with black:
 cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
-Noverlap=read.delim("/prj/JACUSA2_TestField/Nanopore_HEK293/miCLIP2/miCLIP_union_flat_exclude_Y_chromosome.bed",header=F,as.is=T)
+Noverlap=read.delim("/Volumes/prj/JACUSA2_TestField/Nanopore_HEK293/miCLIP2/miCLIP_union_flat_exclude_Y_chromosome.bed",header=F,as.is=T)
 Noverlap.df=data.frame(ID=paste0(Noverlap[,1],":",Noverlap[,2]-2,"_",Noverlap[,3]+2,":",Noverlap[,6]),Type=Noverlap[,4])
 rownames(Noverlap.df)=Noverlap.df[,1]
 
@@ -21,27 +21,27 @@ dataGG=data.frame(AllSitesScores[c(ins,NoCLIPsites),-c(ncol(AllSitesScores)-1,nc
 NumberOfFactors=ncol(AllSitesScores)-2
 colnames(dataGG)=c(paste("NMF",1:NumberOfFactors,sep=""),"CLIP")
 
-rocca<-data.frame(NMF2=dataGG[,"NMF2"],CLIP=c(rep(1,length(ins)),rep(0,length(NoCLIPsites))))
 library(ggplot2)
 require(plotROC)
-p<-ggplot(rocca, aes(m = NMF2, d = CLIP)) + geom_roc(labels=F)
-#color = Base_1
-p<-p+geom_abline(slope=1,intercept=0)
-p
-ggsave("roc.pdf")
-#ok, kinda works
-calc_auc(p)
 
 for(dude in colnames(dataGG)[1:NumberOfFactors])
     {
-xdensity <- ggplot(dataGG, aes_string(dude, color="CLIP")) + 
-stat_ecdf() +
-scale_color_manual(values = cbbPalette) + 
-theme_bw()
+        rocca<-data.frame(NMF2=dataGG[,dude],CLIP=c(rep(1,length(ins)),rep(0,length(NoCLIPsites))))
+        p<-ggplot(rocca, aes(m = NMF2, d = CLIP)) + geom_roc(labels=F)
+        p<-p+geom_abline(slope=1,intercept=0)
+        ggsave(paste0(dude,"_roc.pdf"),device="pdf")
+        calc_auc(p)
+    }
 
+for(dude in colnames(dataGG)[1:NumberOfFactors])
+    {
+        xdensity <- ggplot(dataGG, aes_string(dude, color="CLIP")) + 
+            stat_ecdf() +
+            scale_color_manual(values = cbbPalette) + 
+            theme_bw() + xlim(0, 10)
 #critical need to check 
-ggsave(paste0(dude,"_ecdf.pdf"),device="pdf")
-}
+        ggsave(paste0(dude,"_ecdf.pdf"),device="pdf")
+    }
 
 
 #19:3976465-3976470:- ##

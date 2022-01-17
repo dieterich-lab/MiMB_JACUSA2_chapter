fa3db7138eff8b170c4db9326c2e9371a03dd539
Amina LEM <45292279+AminaLEM@users.noreply.github.com>
2022-01-17 15:51:58 +0100

    Update HEK293_data_prep_step3.R

--- Nanopore_HEK293/Code/HEK293_data_prep_step3.R
+++ Nanopore_HEK293/Code/HEK293_data_prep_step3.R
@@ -1,7 +1,5 @@
 library(NMF)
-library(pROC)
 args = commandArgs(trailingOnly=TRUE)
-
 BigTable=readRDS(paste0(args[1],"/BigTable.rds"))
 res=readRDS(paste0(args[1],"/NMF.rds"));
 ##
@@ -18,6 +16,7 @@ h <- coef(res)
 
 # The palette with black:
 cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
                                         #revcovered pattern
 for(k in 1:nrow(h))
     {
@@ -33,9 +32,7 @@ pdf(paste0(args[1],"/Pattern_Sum_barplot_NMF.pdf"),width=7,height=7)
 
 barplot(rep,legend=F, main=paste0("NMF_Sum_Pattern"),col=cbbPalette,cex.names=2,cex.axis=2)
 dev.off()
-
-
-#                                         #Choice of best pattern for subsequent analysis
+                                        #Choice of best pattern for subsequent analysis
 
 wExtended=data.frame(w,BigTable[rownames(w),"Motif"])
 print("Table of number of best hits for each pattern")
@@ -45,145 +42,50 @@ print(TabPatternInstances)
 #print(paste0("We suggest to use pattern:",chosenPattern))
 tt=apply(wExtended[,1:nrow(h)],1,function(x){which(x==max(x))})
 
-# # Compute CountMatrix                                  
-# CountMatrix<-function(inp,pseudo=0.01){
-# factor3=t(as.matrix((sapply(sapply(inp,strsplit,""),unlist))))
-# factor3=apply(factor3,2,table)
-# #add function
-# factor3Mat=matrix(0,nrow=4,ncol=5)
-# rownames(factor3Mat)<-c("A","C","G","T")
-# colnames(factor3Mat)<-1:5
+#Compute CountMatrix                                  
+CountMatrix<-function(inp,pseudo=0.01){
+factor3=t(as.matrix((sapply(sapply(inp,strsplit,""),unlist))))
+factor3=apply(factor3,2,table)
+#add function
+factor3Mat=matrix(0,nrow=4,ncol=5)
+rownames(factor3Mat)<-c("A","C","G","T")
+colnames(factor3Mat)<-1:5
 
-# for(k in 1:5)
-# {
-#     for(l in names(factor3[[k]]))
-#     {
-#         factor3Mat[l,k]<-as.numeric(factor3[[k]][l])
-#     }
-# }
-# return(factor3Mat)
-# }
+for(k in 1:5)
+{
+    for(l in names(factor3[[k]]))
+    {
+        factor3Mat[l,k]<-as.numeric(factor3[[k]][l])
+    }
+}
+return(factor3Mat)
+}
 
 library(ggplot2)                                        #replace with ggseqlogo
 library(ggseqlogo)
-# for(k in 1:nrow(h))
-#     {
-#          ggseqlogo(BigTable[names(which(tt==k)),"Motif"])
-# #        logomaker(apply(CountMatrix(BigTable[names(which(tt==k)),"Motif"]),2,function(x){x/sum(x)}), type = "EDLogo", bg=apply(CountMatrix(BigTable[names(which(tt!=k)),"Motif"]),2,function(x){x/sum(x)}))
-#         #apply(CountMatrix(BigTable[names(which(tt!=k)),"Motif"]),2,function(x){x/sum(x)})
-#         ggsave(paste0(args[1],"/SeqLogo_",k,"_NMF.pdf"),width=7,height=7,device="pdf")
+for(k in 1:nrow(h))
+    {
+         ggseqlogo(BigTable[names(which(tt==k)),"Motif"])
+#        logomaker(apply(CountMatrix(BigTable[names(which(tt==k)),"Motif"]),2,function(x){x/sum(x)}), type = "EDLogo", bg=apply(CountMatrix(BigTable[names(which(tt!=k)),"Motif"]),2,function(x){x/sum(x)}))
+        #apply(CountMatrix(BigTable[names(which(tt!=k)),"Motif"]),2,function(x){x/sum(x)})
+        ggsave(paste0(args[1],"/SeqLogo_",k,"_NMF.pdf"),width=7,height=7,device="pdf")
        
-#         }
+        }
+
 AllSites=as.matrix(BigTable[,16:30])
 AllSitesScores=AllSites%*%t(h)
-AllSitesScores_sum=AllSites%*% colSums(h)
 
 Noverlap=read.delim(args[2],header=F,as.is=T)
 Noverlap.df=data.frame(ID=paste0(Noverlap[,1],":",Noverlap[,2]-2,"_",Noverlap[,3]+2,":",Noverlap[,6]),Type=Noverlap[,4])
 rownames(Noverlap.df)=Noverlap.df[,1]
 
-ins=intersect(rownames(BigTable),Noverlap.df$ID); #
-ins2= intersect(rownames(BigTable),subset(Noverlap.df$ID,Noverlap.df$Type=="Boulias,Koertel,Koh"))
-ins3= intersect(rownames(BigTable),subset(Noverlap.df$ID,Noverlap.df$Type!="Boulias,Koertel,Koh"))
+ins=intersect(rownames(BigTable),Noverlap.df$ID);
 
-dataGG=data.frame(AllSitesScores[ins,],Noverlap.df[ins,2])
-print(dim(dataGG))
-colnames(dataGG)=c(paste("NMF",1:nrow(h),sep=""),"CLIP")
+# dataGG=data.frame(AllSitesScores[ins,],Noverlap.df[ins,2])
+# colnames(dataGG)=c(paste("Pattern",1:nrow(h),sep=""),"CLIP")
 
-#*************************************BEGIN. Modified part *********************************************
-TestSitesScores = AllSitesScores[!(row.names(AllSitesScores) %in% ins2),]
+colnames(AllSitesScores)=c(paste("Pattern",1:nrow(h),sep=""))
+AllSitesScores=data.frame(AllSitesScores[rownames(BigTable),],TotalScore=apply(AllSitesScores[rownames(BigTable),],1,sum),BigTable[,c("Motif","DRACH")])
 
-dataNG = data.frame(TestSitesScores,rep("NO",dim(TestSitesScores)[1]))
-colnames(dataNG)=c(paste("NMF",1:nrow(h),sep=""),"CLIP")
-dataNG[ins,"CLIP"]=Noverlap.df[ins,2]
-write.csv(dataNG,paste0(args[1],"/wt_ivt.csv"),row.names = FALSE) 
-for(dude in colnames(dataNG)[1:nrow(h)])
-    {
-xdensity <- ggplot(dataNG, aes_string(x=dude, color="CLIP")) + 
-stat_ecdf() +
-scale_color_manual(values = cbbPalette) + 
-theme_bw()
-
-# critical need to check 
-ggsave(paste0(args[1],'/',dude,"_ecdf_3.pdf"),device="pdf")
-}
-
-# for(dude in colnames(dataNG)[1:nrow(h)])
-#     {
-# rocobj = roc(dataNG[,"CLIP"], dataNG[,dude], plot= TRUE)
-# auc <- round(auc(dataNG[,"CLIP"], dataNG[,dude]),4)
-# g = ggroc(rocobj, colour = 'steelblue', size = 2) 
-# g+ ggtitle(paste0(dude,'_ROC Curve ', '(AUC = ', auc, ')'))+    theme_minimal()
-
-# # #critical need to check 
-# ggsave(paste0(args[1],"/", dude,"_roc.pdf"),device="pdf")
-# }
-dataNG = data.frame(AllSitesScores_sum,rep("NO",dim(AllSitesScores_sum)[1]))
-colnames(dataNG)=c("Sum_NMF","CLIP")
-dataNG[ins,"CLIP"]=Noverlap.df[ins,2]
-write.csv(dataNG,paste0(args[1],"/wt_ivt_sum.csv"),row.names = FALSE) 
-
-xdensity <- ggplot(dataNG, aes_string(x='Sum_NMF', color="CLIP")) + 
-stat_ecdf() +
-scale_color_manual(values = cbbPalette) + 
-theme_bw()
-
-#critical need to check 
-ggsave(paste0(args[1],"/NMF_SUM_ecdf_2.pdf"),device="pdf")
-print(dim(TestSitesScores))
-
-# dataGG = data.frame(rowSums(AllSitesScores[ ins3,]),Noverlap.df[ins3,2])
-# colnames(dataGG)=c("Sum_NMF","CLIP")
-# xdensity <- ggplot(dataGG, aes_string(x='Sum_NMF', color="CLIP")) + 
-# stat_ecdf() +
-# scale_color_manual(values = cbbPalette) + 
-# theme_bw()
-
-# #critical need to check 
-# ggsave(paste0(args[1],"/NMF_SUM_ecdf.pdf"),device="pdf")
-
-#*************************************END. Modified part *********************************************
-
-# colnames(AllSitesScores)=c(paste("NMF",1:nrow(h),sep=""))
-# AllSitesScores=data.frame(AllSitesScores[rownames(BigTable),],TotalScore=apply(AllSitesScores[rownames(BigTable),],1,sum),BigTable[,c("Motif","DRACH")])
-
-
-for(dude in colnames(dataGG)[1:nrow(h)])
-    {
-xdensity <- ggplot(dataGG, aes_string(x=dude, color="CLIP")) + 
-stat_ecdf() +
-scale_color_manual(values = cbbPalette) + 
-theme_bw()
-
-#critical need to check 
-ggsave(paste0(args[1],'/',dude,"_ecdf.pdf"),device="pdf")
-}
-
-#*************************************BEGIN. Modified part *********************************************
-# for(dude in colnames(dataNG)[1:nrow(h)])
-#     {
-# xdensity <- ggplot(dataNG, aes_string(x=dude, color="CLIP")) + 
-# stat_ecdf() +
-# scale_color_manual(values = cbbPalette) + 
-# theme_bw()
-
-# #critical need to check 
-# ggsave(paste0(args[1],'/',dude,"_ecdf_2.pdf"),device="pdf")
-# }
-
-
-#*************************************END. Modified part *********************************************
-
-# saveRDS(AllSitesScores,file=paste0(args[1],"/ScoreProfile_NMFall_plusNonCLIP.rds"))
-
-#***************************************ROC curves ***************************************************
-# print(colnames(dataNG))
-
-rocobj = roc(dataNG$CLIP, dataNG$Sum_NMF, plot= TRUE)
-auc <- round(auc(dataNG$CLIP, dataNG$Sum_NMF),4)
-g = ggroc(rocobj, colour = 'steelblue', size = 2) 
-g+ ggtitle(paste0('SUM_NMF ROC Curve ', '(AUC = ', auc, ')'))+    theme_minimal()
-
-#critical need to check 
-ggsave(paste0(args[1],"/SUM_NMF_roc2.pdf"),device="pdf")
-
+saveRDS(AllSitesScores,file=paste0(args[1],"/ScoreProfile_NMFall_plusNonCLIP.rds"))
+#AllSitesScores

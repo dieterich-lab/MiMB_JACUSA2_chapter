18df66837018fe4408759156f9dc131685ba7797
Amina LEM <45292279+AminaLEM@users.noreply.github.com>
2022-02-02 08:45:53 +0100

    Update viz_NMF.R

--- Nanopore_HEK293/scripts/viz_NMF.R
+++ Nanopore_HEK293/scripts/viz_NMF.R
@@ -1,9 +1,6 @@
 library(NMF)
-
 args <- commandArgs(trailingOnly=TRUE)
-
 res<-readRDS(args[1])
-
 colnames(res)<- c("Mismatch_Pos1","Mismatch_Pos2","Mismatch_Pos3","Mismatch_Pos4","Mismatch_Pos5","Deletion_Pos1","Deletion_Pos2","Deletion_Pos3","Deletion_Pos4","Deletion_Pos5","Insertion_Pos1","Insertion_Pos2","Insertion_Pos3","Insertion_Pos4","Insertion_Pos5")
 pdf(args[2])
 layout(cbind(1,2))
@@ -12,8 +9,6 @@ basismap(res)
 # mixture coefficients
 aga<-coefmap(res)
 dev.off()
-
-
 #Choice of best pattern for subsequent analysis
 w<-basis(res)
 print("Table of number of best hits for each pattern")

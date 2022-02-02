fc929862dfa90eb9800fba57b3ce714605b5ac15
Amina Lemsara <alemsara@dieterichlab.org>
2022-02-02 08:40:21 +0100

    finished with reorganization of code

--- Nanopore_HEK293/scripts/viz_pattern.R
+++ Nanopore_HEK293/scripts/viz_pattern.R
@@ -10,21 +10,16 @@ if (dir.exists(snakemake@output[[1]])== FALSE) {
 
 #load data and patterns
 res<-readRDS(snakemake@input[[1]]);
-# colnames(res)<- c("Mismatch_Pos1","Mismatch_Pos2","Mismatch_Pos3","Mismatch_Pos4","Mismatch_Pos5","Deletion_Pos1","Deletion_Pos2","Deletion_Pos3","Deletion_Pos4","Deletion_Pos5","Insertion_Pos1","Insertion_Pos2","Insertion_Pos3","Insertion_Pos4","Insertion_Pos5")
-# pdf(paste0(snakemake@output[[1]],"/NMF_matrices.pdf"))
-# layout(cbind(1,2))
-# # basis components
-# basismap(res)
-# # mixture coefficients
-# aga<-coefmap(res)
-# dev.off()
 w<-basis(res)
+print(dim(w))
 h <- coef(res)
 h <- rbind(h, t(colSums(h)))
 #add the combined patterns
-for (pt in snakemake@params[[2]]){
+for (pt in snakemake@params[[1]]){
     h <- rbind(h, t(colSums(h[pt,])))
     }
+print(dim(h))
+
 # barplots of patterns
 for(k in 1:nrow(h))
     {
@@ -32,16 +27,6 @@ for(k in 1:nrow(h))
     rep1<-matrix(h[k,],ncol=5,byrow=T)[1:3,]
     rownames(rep1)<-c("Mismatch","Deletion","Insertion")
     colnames(rep1)<-c("Pos1","Pos2","Pos3","Pos4","Pos5")
-    barplot(rep1,main<-paste0("NMF_Pattern_",k),col=cbbPalette,cex.names=2,cex.axis=2, legend = TRUE)
+    barplot(rep1,main=paste0("NMF_Pattern_",k),col=cbbPalette,cex.names=2,cex.axis=2, legend = TRUE)
     dev.off()
     }
-
-# #Choice of best pattern for subsequent analysis
-# print("Table of number of best hits for each pattern")
-# TabPatternInstances<-table(apply(w,1,function(x){which(x==max(x))}))
-# print(TabPatternInstances)
-# pdf(paste0(snakemake@output[[1]],"/pattern_scores.pdf"),width=7,height=7)
-# barplot(height=TabPatternInstances, names=1:(dim(w)[2]), main="NMF_Patterns_Scoring",xlab="Patterns", 
-#         ylab="Membership Score (basis matrix)",ylim=range(pretty(c(0, TabPatternInstances))))
-dev.off()
-

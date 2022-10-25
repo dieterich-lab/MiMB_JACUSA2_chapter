456e1a959b44da460606a3bdabbed1675949c653
Amina LEM <45292279+AminaLEM@users.noreply.github.com>
2022-10-25 14:16:42 +0200

    Update get_features.pl

--- Nanopore_HEK293/scripts/get_features.pl
+++ Nanopore_HEK293/scripts/get_features.pl
@@ -7,10 +7,10 @@ while(<>)
 
 	my ($contig, $ref_position, $callScore, $Indel, $base, $site, $strand) = ($tmp[0],$tmp[1]."_".$tmp[2],$tmp[10],0,$tmp[$offset+15], $tmp[7], $tmp[11]);
 	my($deletionScore, $insertionScore) = (0,0);
-	if(/deletion\_score\=([\d\.]+)/)
+	if(/deletion\_score\=([\d\.E-]+)/)
 	{ $deletionScore=$1;}
 
-	if(/insertion\_score\=([\d\.]+)/)
+	if(/insertion\_score\=([\d\.E-]+)/)
 	{ $insertionScore=$1;}
 
 	$site=$site-$tmp[1]+1 if($strand eq '+');
